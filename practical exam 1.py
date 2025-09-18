66a2d0c1fdb8be9fd4414196d384f93901a59237
dev <devdesai475@gmail.com>
2025-09-18 15:52:43 +0530

    Create practical exam 1.py

---
+++ practical exam 1.py
@@ -0,0 +1,18 @@
+
+print("welcome to the Bill spillitor App")
+
+name=input("Enter bill amount")
+
+name=input("Enter number of people")
+
+name=input("Enter tip percentage")
+
+print("would you like to calculate another bill?(y/n):y")
+
+name=print(" total tip amount")
+
+name=print("total bill amount")
+
+name=print("Each person pay")
+
+print("would you like to calculate another bill?(y/n):y")
